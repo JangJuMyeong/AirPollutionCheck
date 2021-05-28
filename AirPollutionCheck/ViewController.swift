34db312526e60174cee047fa9d5f8461eab00c47
JangJuMyeong <isouvezz@gmail.com>
2021-05-28 17:45:06 +0900

    Initial Commit

---
+++ AirPollutionCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirPollutionCheck
+//
+//  Created by 장주명 on 2021/05/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
